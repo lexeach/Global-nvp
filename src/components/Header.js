4b5016ccc75dc8cf3cd27002933a362318844045
lexeach <80155340+lexeach@users.noreply.github.com>
2026-03-03 12:19:05 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ function Header() {
           style={{ height: "50px", width: "50px" }}
         />
         {"         "}
-        GLOBAL NVP
+        EXPONA COIN
       </div>
     </header>
   );
